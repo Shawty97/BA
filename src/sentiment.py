d92246c51f2401380e552f149afa083b3094fb1b
Luca Raimondo <lra@contact.de>
2022-08-22 15:50:36 +0200

    all 3 base sent analyzers working (yay!)

--- src/sentiment.py
+++ src/sentiment.py
@@ -7,16 +7,39 @@ from flair.data import Sentence
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+DECIMAL_PLACES = 3
+
+sia = TextClassifier.load('en-sentiment')
+sid = SentimentIntensityAnalyzer()
+
 
 class SentimentAnalyzer:
-    def flair(text: str):
-        return Sentence(text).to_dict()["labels"][0]["score"]
 
-    def textblob(text: str) -> float:
+    @staticmethod
+    def flair(text: str or list):
+        """returns a float between 0 and 1"""
+        if isinstance(text, list):
+            text = " ".join(text)
+
+        sentence = Sentence(text)
+        sia.predict(sentence)
+        return sentence.labels[0]._score
+
+    @staticmethod
+    def textblob(text: str or list) -> float:
+        """returns a float between -1 and 1"""
+        if isinstance(text, list):
+            text = " ".join(text)
+
         return TextBlob(text).sentiment.polarity
 
-    def vader(text: str) -> float:
-        return SentimentIntensityAnalyzer().polarity_scores(text)["compound"]
+    @staticmethod
+    def vader(text: str or list) -> float:
+        """returns a float between -1 and 1"""
+        if isinstance(text, list):
+            text = " ".join(text)
+
+        return sid.polarity_scores(text)['compound']
 
 
 def sentiment_json(file_path: Path, method_id: str = "all"):
@@ -35,9 +58,13 @@ def sentiment_json(file_path: Path, method_id: str = "all"):
         tweets = json.load(file_in)
 
         # make sentiment analysis and add it to the attributes
-        for method in methods:
-            for tweet in tweets:
-                tweet["sentiment_scores"][method_id] = method(tweet["text"])
+        for tweet in tweets:
+            tweet["sentiment_scores"] = {}
+
+            for method in methods:
+                tweet["sentiment_scores"][method.__name__] = '{:.2f}'.format(method(tweet["text"]))
+            
+            print(tweet["tweet_id"], tuple(tweet["sentiment_scores"].values()))
 
     # write analyzed data
     with open(file_path, mode="w", encoding="utf-8") as file_out:
