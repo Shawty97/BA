33dcf271724efd01d1cf2222d9466d80f0c06dba
Luca Raimondo <lra@contact.de>
2022-09-23 15:19:00 +0200

    added sums of sented tweets

--- src/compstat.py
+++ src/compstat.py
@@ -3,6 +3,9 @@ import json
 from tqdm import tqdm
 from pathlib import Path
 
+NEGATIVE_UPPER_BOUND = -0.3
+NEUTRAL_UPPER_BOUND = 0.3
+
 
 def merge_company_status_files(
     file_companies: Path, file_analyzed: Path, out_path: Path
@@ -14,16 +17,16 @@ def merge_company_status_files(
         reader = csv.reader(file_in, delimiter=",")
         _ = next(reader)
 
-        print('Step 1: Load company data')
+        print("Step 1: Load company data")
         for row in tqdm(reader):
-            new_company_info_vectors[row[2]] = [[], [], [], []]
+            new_company_info_vectors[row[2]] = [[], [], [], [], -1, -1, -1]
 
     # load tweets for the companies
     with open(file_analyzed, encoding="utf-8") as file_in:
         reader = csv.reader(file_in, delimiter=",")
         _ = next(reader)
 
-        print('Step 2: Load tweets')
+        print("Step 2: Load tweets")
         for row in tqdm(reader):
             company_name = row[18]
             if company_name in new_company_info_vectors:
@@ -38,19 +41,39 @@ def merge_company_status_files(
 
                 # reply count
                 new_company_info_vectors[company_name][3].append(int(row[11]))
+    
+    
+    # calculate sums
+    for company in new_company_info_vectors.values():
+        # positive tweets
+        company[4] = len([x for x in company[0] if x >= NEUTRAL_UPPER_BOUND])
+
+        # neutral tweets
+        company[6] = len([x for x in company[0] if x >= NEGATIVE_UPPER_BOUND and x < NEUTRAL_UPPER_BOUND])
+
+        # negative tweets
+        company[5] = len([x for x in company[0] if x < NEGATIVE_UPPER_BOUND])
 
     # load company data
     with open(file_companies, encoding="utf-8") as file_in:
         reader = csv.reader(file_in, delimiter=",")
         header = next(reader)
 
-        print('Step 3: Write merged data')
+        print("Step 3: Write merged data")
         # write merged data
         with open(out_path, mode="w", encoding="utf-8", newline="") as file_out:
             writer = csv.writer(file_out, delimiter=",")
             writer.writerow(
                 header
-                + ["sent_vader_vector", "retweet_counts", "like_counts", "reply_counts"]
+                + [
+                    "sent_vader_vector",
+                    "retweet_counts",
+                    "like_counts",
+                    "reply_counts",
+                    "pos_tweet_count",
+                    "neg_tweet_count",
+                    "neutr_tweet_count",
+                ]
             )
 
             for row in tqdm(reader):
