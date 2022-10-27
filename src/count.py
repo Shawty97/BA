4a9e81919105d8c2a46f6134cd9e55310fbd7be4
Luca Raimondo <lra@contact.de>
2022-10-27 13:37:11 +0200

    added encoding

--- src/count.py
+++ src/count.py
@@ -2,12 +2,13 @@ from collections import Counter
 from pathlib import Path
 import pandas
 
+
 def count_words(file_path: Path) -> Counter:
-    with open(file_path) as file_in:
+    with open(file_path, encoding="utf-8") as file_in:
         df = pandas.read_csv(file_in)
 
         counter = Counter()
         for text in df["text"]:
-            counter += Counter(text.split(' '))
+            counter += Counter(text.split(" "))
 
         return counter
